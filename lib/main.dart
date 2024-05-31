5c25740da905803ae0d368cee0d5df349fdfa3f5
okane <okanekudasai6@gmail.com>
2024-05-31 17:24:51 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:untitled3/src/game_app.dart';
+
+
+void main() {
+  runApp(const GameApp());
+}
---
+++ lib/src/components/components.dart
@@ -0,0 +1,4 @@
+export 'play_area.dart';
+export 'bat.dart';
+export 'brick.dart';
+export 'ball.dart';
